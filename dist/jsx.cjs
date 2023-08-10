100acc4b77147a689a8cc326172f7afb48de5746
Arnaud Juracek <arnaud.juracek@gmail.com>
2023-08-10 13:11:08 +0200

    Add dist to .gitignore
    Dist will be force added when deploying

--- dist/jsx.cjs
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});const e=require("./Component-5ef97f35.cjs");function r(n,t){return function(o){n[t]=o}}function l(n,t){return e.h(n.nodeName,e.extend(e.extend({},n.props),t),arguments.length>2?[].slice.call(arguments,2):n.children)}exports.Component=e.Component;exports.h=e.h;exports.render=e.render;exports.addRef=r;exports.cloneElement=l;exports.ref=r;
-//# sourceMappingURL=jsx.cjs.map
--- dist/state.cjs
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});const e=require("./ensure-1c6e4abe.cjs"),r=d=>e.derived(d,a=>!a);exports.d=e.derived;exports.derived=e.derived;exports.ensure=e.ensure;exports.r=e.readable;exports.readable=e.readable;exports.signal=e.signal;exports.w=e.writable;exports.writable=e.writable;exports.n=r;exports.not=r;
-//# sourceMappingURL=state.cjs.map

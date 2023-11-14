94760f1c06cdf4d61796a2a104def7a0cd3dba79
Arnaud Juracek <arnaud.juracek@gmail.com>
2023-11-14 18:35:23 +0100

    Handle <Input type=file>

--- lib/components/Input.jsx
+++ lib/components/Input.jsx
@@ -13,13 +13,18 @@ export default class Input extends Component {
     this.handleFocus = this.handleFocus.bind(this)
 
     this.state = {
+      value: ensure(writable)(props['store-value'], props.value),
+      files: ensure(writable)(props['store-files'], props.files),
+
       placeholder: ensure(writable)(props['store-placeholder'], props.placeholder),
       title: ensure(writable)(props['store-title'], props.title),
+      label: ensure(writable)(props['store-label'], props.label),
       before: ensure(writable)(props['store-before'], props.before),
       after: ensure(writable)(props['store-after'], props.after),
       icon: ensure(writable)(props['store-icon'], props.icon),
 
-      value: ensure(writable)(props['store-value'], props.value),
+      accept: ensure(writable)(props['store-accept'], props.accept ?? '*'),
+      multiple: ensure(writable)(props['store-multiple'], props.multiple ?? '*'),
       min: ensure(writable)(props['store-min'], props.min),
       max: ensure(writable)(props['store-max'], props.max),
       step: ensure(writable)(props['store-step'], props.step),
@@ -37,13 +42,17 @@ export default class Input extends Component {
         id={props.id}
         tabIndex={props.tabindex}
         class={classnames(style.input, props.class)}
+        data-type={props.type}
         store-title={state.title}
         store-class-has-icon={state.icon}
         store-class-is-active={state.active}
         store-class-is-disabled={state.disabled}
         store-class-is-hidden={state.hidden}
         store-class-is-waiting={state.waiting}
-        event-click={e => this.refs.input.focus()}
+        event-click={e => {
+          if (props.type === 'file') this.refs.input.click()
+          else this.refs.input.focus()
+        }}
         event-mouseenter={e => (props['event-mouseenter'] ?? noop)(e, this)}
         event-mouseleave={e => (props['event-mouseleave'] ?? noop)(e, this)}
       >
@@ -52,15 +61,17 @@ export default class Input extends Component {
           class={style.input__icon}
           store-innerHTML={state.icon}
         />
+        <label class={style.input__label} store-innerHTML={state.label} />
         <label class={style.input__before} store-innerHTML={state.before} />
         <input
           ref={this.ref('input')}
           type={props.type}
-          store-value={state.value}
-          store-min={state.min}
-          store-max={state.max}
-          store-step={state.step}
+          store-min={props.type === 'number' ? state.min : undefined}
+          store-max={props.type === 'number' ? state.max : undefined}
+          store-step={props.type === 'number' ? state.step : undefined}
           store-placeholder={state.placeholder}
+          store-accept={props.type === 'file' ? state.accept : undefined}
+          store-multiple={props.type === 'file' ? state.multiple : undefined}
           store-disabled={state.disabled}
           event-input={this.handleInput}
           event-focus={this.handleFocus}
@@ -76,6 +87,24 @@ export default class Input extends Component {
   }
 
   update () {
+    const value = this.state.value.get()
+
+    switch (this.props.type) {
+      case 'number': {
+        this.refs.input.value = +value
+        break
+      }
+
+      case 'file': {
+        this.refs.input.files = value
+        break
+      }
+
+      default: {
+        this.refs.input.value = value
+      }
+    }
+
     if (this.props.autoSize) {
       const length = (String(this.refs.input.value) ?? '').length || (this.state.placeholder.current ?? '').length || 1
       this.refs.input.style.width = (length + 1) + 'ch'
@@ -85,10 +114,14 @@ export default class Input extends Component {
   async handleInput (e) {
     if (this.state.waiting.get()) return e.preventDefault()
 
-    const value = this.refs.input.value
-
     this.state.waiting.set(true)
-    this.state.value.set(this.props.type === 'number' ? +value : value)
+
+    switch (this.props.type) {
+      case 'number': this.state.value.set(+this.refs.input.value); break
+      case 'file': this.state.value.set(this.refs.input.files); break
+      default: this.state.value.set(this.refs.input.value)
+    }
+
     await (this.props['event-input'] ?? noop)(e, this)
 
     // Testing for mounted because event-click may have destroyed this component

9f475fb222e972d17948639cc9bd3b0bebfb8f7c
Arnaud Juracek <arnaud.juracek@gmail.com>
2024-02-28 12:47:11 +0100

    Release 1.0.0

--- dist/ensure-e91d6c46.js
+++ dist/ensure-e91d6c46.js
@@ -1,32 +1,32 @@
-function f(i, t, s, e) {
-  this.fn = t, this.ctx = s || null, this.owner = i, this.once = !!e;
+function f(i, t, e, s) {
+  this.fn = t, this.ctx = e || null, this.owner = i, this.once = !!s;
 }
 function c(i, t) {
   t.prev && (t.prev.next = t.next), t.next && (t.next.prev = t.prev), t.ctx = t.fn = t.owner = null, t === i._first && (i._first = t.next), t === i._last && (i._last = t.prev);
 }
 class h {
   constructor() {
-    this._first = this._last = null;
+    this._first = this._last = null, this._symbol = Symbol("signal");
   }
-  dispatch(t, s, e, r, l) {
+  dispatch(t, e, s, r, l) {
     let u = this._first;
     for (; u; )
-      u.fn.call(u.ctx, t, s, e, r, l), u.once && this.unsubscribe(u), u = u.next;
+      u.fn.call(u.ctx, t, e, s, r, l), u.once && this.unsubscribe(u), u = u.next;
   }
-  subscribe(t, s, e) {
-    const r = new f(this, t, s, e);
+  subscribe(t, e, s) {
+    const r = new f(this, t, e, s);
     return this._first ? (this._last.next = r, r.prev = this._last, this._last = r) : (this._first = r, this._last = r), r;
   }
-  subscribeOnce(t, s) {
-    return this.subscribe(t, s, !0);
+  subscribeOnce(t, e) {
+    return this.subscribe(t, e, !0);
   }
-  unsubscribe(t, s) {
+  unsubscribe(t, e) {
     if (t instanceof f)
       return c(this, t);
-    s || (s = null);
-    let e = this._first;
-    for (; e; )
-      e.fn === t && e.ctx === s && c(this, e), e = e.next;
+    e || (e = null);
+    let s = this._first;
+    for (; s; )
+      s.fn === t && s.ctx === e && c(this, s), s = s.next;
   }
   unsubscribeAll() {
     let t = this._first;
@@ -34,7 +34,7 @@ class h {
       c(this, t), t = t.next;
   }
 }
-function b() {
+function x() {
   return new h();
 }
 class a extends h {
@@ -55,13 +55,13 @@ class o extends a {
   constructor(t) {
     super(t), this.toggle = this.toggle.bind(this), this.reset = this.reset.bind(this);
   }
-  set(t, s) {
-    if (!s && this.current === t)
+  set(t, e) {
+    if (!e && this.current === t)
       return;
     this.previous = this.current, this.current = t;
-    let e = this._first;
-    for (; e; )
-      e.fn.call(e.ctx, this.current, this.previous), e.once && this.unsubscribe(e), e = e.next;
+    let s = this._first;
+    for (; s; )
+      s.fn.call(s.ctx, this.current, this.previous), s.once && this.unsubscribe(s), s = s.next;
   }
   reset() {
     this.set(this.initial);
@@ -69,23 +69,23 @@ class o extends a {
   toggle() {
     this.set(!this.current);
   }
-  update(t, s) {
-    const e = t(this.current);
-    this.set(e !== void 0 ? e : this.current, s);
+  update(t, e) {
+    const s = t(this.current);
+    this.set(s !== void 0 ? s : this.current, e);
   }
 }
-function p(i) {
+function g(i) {
   return new o(i);
 }
-function w(i, t) {
-  const s = new o(), e = s.set.bind(s);
-  delete s.set;
+function p(i, t) {
+  const e = new o(), s = e.set.bind(e);
+  delete e.set;
   const r = Array.isArray(i);
   let l = r ? new Array(i.length) : null;
   if (r)
     for (let n = 0, _ = i.length; n < _; n++)
-      l[n] = i[n].current, i[n].subscribe(function(x) {
-        l[n] = x, u();
+      l[n] = i[n].current, i[n].subscribe(function(b) {
+        l[n] = b, u();
       });
   else
     l = i.current, i.subscribe(function(n) {
@@ -94,24 +94,24 @@ function w(i, t) {
   u();
   function u() {
     const n = t(l);
-    n && n.then ? n.then(e) : e(n);
+    n && n.then ? n.then(s) : s(n);
   }
-  return s;
+  return e;
 }
-const g = (i) => (...t) => {
-  for (const s of t) {
-    if (s instanceof h)
-      return s;
-    if (s !== void 0)
-      return i(s);
+const w = (i) => (...t) => {
+  for (const e of t) {
+    if (e instanceof h)
+      return e;
+    if (e !== void 0)
+      return i(e);
   }
   return i(null);
 };
 export {
-  w as d,
-  g as e,
+  p as d,
+  w as e,
   d as r,
-  b as s,
-  p as w
+  x as s,
+  g as w
 };
-//# sourceMappingURL=ensure-5f6c54f2.js.map
+//# sourceMappingURL=ensure-e91d6c46.js.map

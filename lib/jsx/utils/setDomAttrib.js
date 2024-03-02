tooooools/ui

commit 89ff4ae1e688564ce3af8f06dc8d5c3f832950aa

Improve JSX syntax handling
- Signals can now be used implicitly as a value, ie without the store-
  prefix
- Events can no be set as attributes using their native onevent syntax
- The style prop now support using signal as property value

lib/jsx/utils/setDomAttrib.js
@@ -1,8 +1,6 @@
 // Some portions of code are taken from preact
 // https://github.com/developit/preact/blob/master/src/dom/index.js
 
-import extend from './extend'
-
 const namespaces = [
   'xlink',
   'xmlns',
@@ -88,24 +86,34 @@ function setDomAttrib (el, name, value, collector, isSvg) {
     const className = name.substring(12)
     const fn = updateClass(className)
     collector.append({ storeEvents: [{ store: value, init: true, fn, el }] })
-  } else if (startsWith(name, 'store-')) {
-    let attrib = name.substring(6)
+  } else if (startsWith(name, 'store-') || String(value._symbol) === 'Symbol(signal)') {
+    let attrib = startsWith(name, 'store-') ? name.substring(6) : name
     if (attrib === 'text') attrib = 'textContent'
     const fn = updateAttrib(attrib, isSvg)
     collector.append({ storeEvents: [{ store: value, init: true, fn, el }] })
   } else if (typeof value === 'function' && startsWith(name, 'on')) {
-
     // Event attribute (usually do not use this, prefer the synthetic ones)
-    // const eventType = name.toLowerCase();
-    // el[ eventType ] = value;
-
+    const eventType = name.toLowerCase()
+    el[eventType] = value
   } else if (name === 'class' && !isSvg) {
     // Class - only for non-svg node
     el.className = value || ''
   } else if (name === 'style') {
     // Style attribute
-    if (typeof value === 'object') extend(el.style, value)
-    else if (typeof value === 'string') el.style.cssText = value
+    if (typeof value === 'object') {
+      for (const key in value) {
+        const updateProp = v => {
+          if (key.startsWith('--')) el.style.setProperty(key, v)
+          else { el.style[key] = v }
+        }
+
+        if (String(value[key]._symbol) === 'Symbol(signal)') {
+          collector.append({ storeEvents: [{ store: value[key], init: true, fn: updateProp, el }] })
+        } else {
+          updateProp(value)
+        }
+      }
+    } else if (typeof value === 'string') el.style.cssText = value
   } else if (name !== 'list' && name !== 'type' && !isSvg && name in el) {
     // Attrib
     // Attempt to set an attribute to the given value.

lib/state/signal.js
@@ -16,6 +16,7 @@ function removeNode (owner, node) {
 class Signal {
   constructor () {
     this._first = this._last = null
+    this._symbol = Symbol('signal')
   }
 
   dispatch (a0, a1, a2, a3, a4) {

commit 9618b2bb1bdb38bbe0220cb4945fdd7168adbfc7

Fix regression introduced in 89ff4ae

lib/jsx/utils/setDomAttrib.js
@@ -86,7 +86,7 @@ function setDomAttrib (el, name, value, collector, isSvg) {
     const className = name.substring(12)
     const fn = updateClass(className)
     collector.append({ storeEvents: [{ store: value, init: true, fn, el }] })
-  } else if (startsWith(name, 'store-') || String(value._symbol) === 'Symbol(signal)') {
+  } else if (startsWith(name, 'store-') || String((value || {})._symbol) === 'Symbol(signal)') {
     let attrib = startsWith(name, 'store-') ? name.substring(6) : name
     if (attrib === 'text') attrib = 'textContent'
     const fn = updateAttrib(attrib, isSvg)
@@ -107,7 +107,7 @@ function setDomAttrib (el, name, value, collector, isSvg) {
           else { el.style[key] = v }
         }
 
-        if (String(value[key]._symbol) === 'Symbol(signal)') {
+        if (String((value || [])[key]._symbol) === 'Symbol(signal)') {
           collector.append({ storeEvents: [{ store: value[key], init: true, fn: updateProp, el }] })
         } else {
           updateProp(value)
a182dad114663b9be7f966de641db1defbceca42
Arnaud Juracek <arnaud.juracek@gmail.com>
2024-11-21 15:05:35 +0100

    Release 1.5.2

--- dist/Component-7ec09343.js
+++ dist/Component-7ec09343.js
@@ -1,18 +1,18 @@
-function m(s, t) {
+function D(s, t) {
   const e = {};
   return e.vnode = !0, e.type = s, e.props = t || {}, e.children = [].concat.apply([], [].slice.call(arguments, 2)), e.isInstance = s && s.template && typeof s.template == "function", e.isComponent = typeof s == "function" && s.prototype && typeof s.prototype.render == "function", e.isStateless = typeof s == "function" && !e.isComponent, e.isDom = typeof s == "string", e;
 }
-m.f = (s) => s.children;
+D.f = (s) => s.children;
 function w(s, t) {
   for (const e in t)
     s[e] = t[e];
   return s;
 }
-function g() {
+function y() {
 }
-function D(s, t, e, n) {
+function m(s, t, e, n) {
   if (n)
-    return { log: g, warn: g, error: g };
+    return { log: y, warn: y, error: y };
   const i = [];
   s = s.toUpperCase(), i.push(`%c ${s} `);
   let o = "font-weight:bold;";
@@ -22,12 +22,12 @@ function D(s, t, e, n) {
     error: console.error.bind(console, ...i)
   };
 }
-const C = ["nodes", "components", "refs", "domEvents", "storeEvents"];
-class a {
+const A = ["nodes", "components", "refs", "domEvents", "storeEvents"];
+class d {
   constructor() {
     this.data = {};
-    for (let t = 0; t < C.length; t++)
-      this.data[C[t]] = [];
+    for (let t = 0; t < A.length; t++)
+      this.data[A[t]] = [];
   }
   append(t) {
     for (const e in t)
@@ -41,7 +41,7 @@ class a {
     return this.data;
   }
 }
-const h = [
+const p = [
   "xlink",
   "xmlns",
   "xml"
@@ -53,9 +53,9 @@ const h = [
   "webkit-playsinline",
   "playsinline",
   "selected"
-]), y = (s) => s && String(s._symbol) === "Symbol(signal)";
+]), g = (s) => s && String(s._symbol) === "Symbol(signal)";
 let _;
-const l = (s, t) => s.substr(0, t.length) === t, A = (s) => (t, e) => e.classList.toggle(s, t || !1), b = (s = "textContent", t) => (e, n) => {
+const l = (s, t) => s.substr(0, t.length) === t, C = (s) => (t, e) => e.classList.toggle(s, t || !1), b = (s = "textContent", t) => (e, n) => {
   if (t && s !== (s = s.replace(/^xlink:?/, "")))
     n.setAttributeNS("http://www.w3.org/1999/xlink", s.toLowerCase(), e);
   else {
@@ -69,10 +69,10 @@ const l = (s, t) => s.substr(0, t.length) === t, A = (s) => (t, e) => e.classLis
 function M(s, t, e, n, i) {
   if (e !== void 0 && !(t === "__self" || t === "__source")) {
     t === "className" && (t = "class");
-    for (let o = 0; o < h.length; o++)
-      if (l(t, h[o])) {
-        const r = h[o].length;
-        t[r] && t[r] !== ":" && (t = h[o] + ":" + t[r].toLowerCase() + t.substr(r + 1, t.length));
+    for (let o = 0; o < p.length; o++)
+      if (l(t, p[o])) {
+        const r = p[o].length;
+        t[r] && t[r] !== ":" && (t = p[o] + ":" + t[r].toLowerCase() + t.substr(r + 1, t.length));
         break;
       }
     if (t !== "ref")
@@ -82,14 +82,14 @@ function M(s, t, e, n, i) {
       } else if (t === "store") {
         const o = Array.isArray(e) ? e : [e], r = [];
         for (let c = 0, f = o.length; c < f; c++) {
-          const u = Array.isArray(o[c]) ? o[c] : [o[c]], E = u[0], R = u[1], k = u[2] !== void 0 ? u[2] : !0;
+          const h = Array.isArray(o[c]) ? o[c] : [o[c]], E = h[0], R = h[1], k = h[2] !== void 0 ? h[2] : !0;
           E && r.push({ store: E, fn: R, init: k, el: s });
         }
         n.append({ storeEvents: r });
       } else if (l(t, "store-class-")) {
-        const o = t.substring(12), r = A(o);
+        const o = t.substring(12), r = C(o);
         n.append({ storeEvents: [{ store: e, init: !0, fn: r, el: s }] });
-      } else if (l(t, "store-") || y(e)) {
+      } else if (l(t, "store-") || g(e)) {
         let o = l(t, "store-") ? t.substring(6) : t;
         o === "text" && (o = "textContent");
         const r = b(o, i);
@@ -103,8 +103,8 @@ function M(s, t, e, n, i) {
             const r = e[o];
             if (typeof r == "string" && s.classList.add(...r.split(" ")), typeof e == "object") {
               for (const c in r)
-                if (typeof r[c] == "boolean" && s.classList.toggle(c, r[c]), y(r[c])) {
-                  const f = A(c);
+                if (typeof r[c] == "boolean" && s.classList.toggle(c, r[c]), g(r[c])) {
+                  const f = C(c);
                   n.append({ storeEvents: [{ store: r[c], init: !0, fn: f, el: s }] });
                 }
             }
@@ -115,7 +115,7 @@ function M(s, t, e, n, i) {
             const r = (c) => {
               o.startsWith("--") ? s.style.setProperty(o, c) : s.style[o] = c;
             };
-            y((e ?? [])[o]) ? n.append({ storeEvents: [{ store: e[o], init: !0, fn: r, el: s }] }) : r(e[o]);
+            g((e ?? [])[o]) ? n.append({ storeEvents: [{ store: e[o], init: !0, fn: r, el: s }] }) : r(e[o]);
           }
         else
           typeof e == "string" && (s.style.cssText = e);
@@ -131,7 +131,7 @@ function M(s, t, e, n, i) {
 function P(s) {
   return Array.isArray(s) ? s : [s];
 }
-function p(s, t) {
+function u(s, t) {
   if (Array.isArray(s))
     return T(s, t);
   if (s instanceof window.Element)
@@ -153,21 +153,21 @@ function p(s, t) {
 }
 function T(s, t) {
   s = P(s);
-  const e = new a();
+  const e = new d();
   for (let n = 0; n < s.length; n++)
-    e.append(p(s[n], t));
+    e.append(u(s[n], t));
   return e.get();
 }
 function B(s) {
-  const t = new a();
+  const t = new d();
   return s == null || s === !1 || t.append({ nodes: document.createTextNode(s + "") }), t.get();
 }
 function j(s, t, e) {
-  const n = new a();
+  const n = new d();
   let i = [];
   s.type === "svg" && (e = !0);
   for (let r = 0; r < t.children.length; r++) {
-    const c = p(t.children[r], e);
+    const c = u(t.children[r], e);
     c.nodes && (i = i.concat(c.nodes), n.append({
       components: c.components,
       refs: c.refs,
@@ -183,18 +183,18 @@ function j(s, t, e) {
   return t.ref && n.append({ refs: { ref: o, fn: t.ref } }), n.append({ nodes: o }), n.get();
 }
 function W(s) {
-  const t = new a();
+  const t = new d();
   return t.append({ nodes: s }), t.get();
 }
 function $(s, t, e) {
-  const n = new a(), i = s.type(t);
-  return n.append(p(i, e)), n.get();
+  const n = new d(), i = s.type(t);
+  return n.append(u(i, e)), n.get();
 }
 function L(s, t, e, n = !1) {
-  const i = new a(), o = n ? s.type : new s.type(t);
+  const i = new d(), o = n ? s.type : new s.type(t);
   n && (t = o.props), o.beforeRender(t);
   const r = o.template(t, o.state || {}) || t.children;
-  t.ref && i.append({ refs: { ref: o, fn: t.ref } }), i.append(p(r, e));
+  t.ref && i.append({ refs: { ref: o, fn: t.ref } }), i.append(u(r, e));
   const c = i.data.nodes;
   o.base = c.length > 1 ? c : c[0];
   for (const f in i.data)
@@ -209,11 +209,11 @@ function L(s, t, e, n = !1) {
     o._collector.components[f]._parent = o;
   return i.get();
 }
-function d(s, t, e) {
+function a(s, t, e) {
   const n = s._collector && s._collector.components;
   if (e && t(s), n)
     for (let i = 0; i < n.length; i++)
-      d(n[i], t);
+      a(n[i], t);
   e || t(s);
 }
 function H(s) {
@@ -236,10 +236,10 @@ function O(s) {
   }
 }
 function S(s) {
-  d(s, H);
+  a(s, H);
 }
 function U(s) {
-  d(s, O), d(s, I);
+  a(s, O), a(s, I);
 }
 const Y = /* @__PURE__ */ new Set([
   "g",
@@ -249,9 +249,9 @@ const Y = /* @__PURE__ */ new Set([
 ]);
 function q(s, t = document.body, e) {
   let n = 0;
-  const i = p(s, t && Y.has(t.tagName));
+  const i = u(s, t && Y.has(t.tagName));
   let o = { _collector: i };
-  if (S(o), d(o, function(r) {
+  if (S(o), a(o, function(r) {
     r.afterRender && r.afterRender(r.props);
   }), typeof t == "function") {
     const r = i.nodes.length < 2 ? i.nodes[0] : i.nodes;
@@ -262,7 +262,7 @@ function q(s, t = document.body, e) {
   else if (typeof t == "object" && t !== null && t.insertBefore)
     for (n = 0; n < i.nodes.length; n++)
       t.insertBefore.parentNode.insertBefore(i.nodes[n], t.insertBefore);
-  if ((!e || e.mounted) && (U(o), d(o, function(r) {
+  if ((!e || e.mounted) && (U(o), a(o, function(r) {
     r.mounted = !0, r.afterMount && r.afterMount(r.props);
   })), e && e._collector) {
     const r = e._collector;
@@ -275,13 +275,13 @@ function q(s, t = document.body, e) {
 class N {
   static flatten(t) {
     let e = [];
-    for (const n of t.children)
+    for (const n of Array.isArray(t) ? t : t.children ?? [])
       e = e.concat(n, N.flatten(n));
     return e;
   }
   constructor(t = {}) {
     this._parent = null, this._collector = { refs: [], components: [], domEvents: [], storeEvents: [] }, this._storeListeners = [], this.refs = {}, this.state = {}, this.store = {}, this.props = t || {}, this.base = null, this.mounted = !1, this.destroyed = !1;
-    const e = t.name || this.constructor.name || "Component", { log: n, warn: i, error: o } = D(
+    const e = t.name || this.constructor.name || "Component", { log: n, warn: i, error: o } = m(
       e,
       "white",
       "#000",
@@ -381,8 +381,8 @@ class N {
 export {
   N as C,
   w as e,
-  m as h,
-  g as n,
+  D as h,
+  y as n,
   q as r
 };
-//# sourceMappingURL=Component-af0a7988.js.map
+//# sourceMappingURL=Component-7ec09343.js.map
